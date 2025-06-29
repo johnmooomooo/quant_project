ce54f4aca35e9791d2be6edd90b6b05324120615
user001 <user001@gmail.com>
2025-06-29 17:44:38 +0800

    fix 1

--- strategies/ma_strategy.py
+++ strategies/ma_strategy.py
@@ -5,24 +5,23 @@ class MAStrategy:
         self.fast = fast
         self.slow = slow
 
-    def generate_signal(self, df):
-        df['ma_fast'] = df['Close'].rolling(self.fast).mean()
-        df['ma_slow'] = df['Close'].rolling(self.slow).mean()
-        signal = []
-        for i in range(len(df)):
-            if i < self.slow:
-                signal.append(0)
-            elif (
-                df['ma_fast'].iloc[i-1] < df['ma_slow'].iloc[i-1]
-                and df['ma_fast'].iloc[i] > df['ma_slow'].iloc[i]
-            ):
-                signal.append(1)  # buy
-            elif (
-                df['ma_fast'].iloc[i-1] > df['ma_slow'].iloc[i-1]
-                and df['ma_fast'].iloc[i] < df['ma_slow'].iloc[i]
-            ):
-                signal.append(-1)  # sell
-            else:
-                signal.append(0)
-        df['ma_signal'] = signal
-        return df
+        def generate_signal(self, df):
+            df['ma_fast'] = df['Close'].rolling(self.fast).mean()
+            df['ma_slow'] = df['Close'].rolling(self.slow).mean()
+            signal = []
+            for i in range(len(df)):
+                if i < self.slow:
+                    signal.append(0)
+                elif df['ma_fast'].iloc[i-1] <= df['ma_slow'].iloc[i-1] and df['ma_fast'].iloc[i] > df['ma_slow'].iloc[i]:
+                    signal.append(1)
+                elif df['ma_fast'].iloc[i-1] >= df['ma_slow'].iloc[i-1] and df['ma_fast'].iloc[i] < df['ma_slow'].iloc[i]:
+                    signal.append(-1)
+                else:
+                    signal.append(0)
+            df['ma_signal'] = signal
+
+            # 🌟 添加打印，验证有无交叉
+            print(df[['Close','ma_fast','ma_slow','ma_signal']].tail(50))
+
+            return df
+
