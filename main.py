e232795d5a48e057923fc5149ea55a4dba56801d
user001 <user001@gmail.com>
2025-06-29 19:23:05 +0800

    fix 1

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv("AAPL.csv", index_col=0, parse_dates=True)
+df = pd.read_csv("AAPL.csv", index_col=0, parse_dates=True, skiprows=[1,2])
+df = df[["Close","Volume"]].dropna()
+df = df.astype(float)
 
 for i in range(5, len(df)):
     drop_pct = (df['Close'].iloc[i-1] - df['Close'].iloc[i]) / df['Close'].iloc[i-1]
     avg_vol = df['Volume'].iloc[i-5:i].mean()
-    if (
-        drop_pct >= 0.05
-    ):
+    if drop_pct >= 0.05:
         print(f"{df.index[i]}: drop={drop_pct:.2%}, vol={df['Volume'].iloc[i]:.0f}, avg_vol={avg_vol:.0f}")
